8c8e0edf6122019ea0358d5485ad852a52ed568d
Jannis Gebauer <ja.geb@me.com>
2017-03-14 16:38:50 +0100

    beefed up the release process

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-pyinstaller --onefile app.spec
-cp dist/app app-skeleton/safetybar.app/Contents/MacOS/safetybar
-rm dist/safetybar.dmg
-appdmg app-skeleton/appdmg.json dist/safetybar.dmg
